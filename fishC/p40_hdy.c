dbfea57ac8820b1a99de587c185ab5ca127a434a
ShunJie <xiaoshunjie@qq.com>
2023-08-11 23:08:33 +0800

    高级宏定义

--- fishC/p40_hdy.c
+++ fishC/p40_hdy.c
@@ -17,8 +17,27 @@ void Test1(void)
     double s;
     printf("Please enter circular' radius:\n");
     scanf("%lf", &r);
+    // #undef PI // 支持去宏定义
     s = r * r * PI;
     printf("Circular area: %.2lf\n", s);
+}
+// 求个体积
+#define R 6613
+
+// 带参数的宏定义
+#define MAX(x, y) (((x) > (y)) ? (x) : (y))
+void Test2(void)
+{
+    int x, y;
+    printf("Please enter two int:");
+    scanf("%d%d", &x, &y);
+    printf("%d is the Max", MAX(x, y));
+}
+
+#define squre(x) ((x) * (x))
+void Test3(void)
+{
+    
 }
 int main()
 {
