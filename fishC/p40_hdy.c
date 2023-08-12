c0fc1fcbb7860c7dedf066241a7fb45fdaeb28e0
ShunJie <xiaoshunjie@qq.com>
2023-08-12 23:34:51 +0800

    补全宏定义代码

--- fishC/p40_hdy.c
+++ fishC/p40_hdy.c
@@ -9,8 +9,8 @@
  * 
  */
 #include <stdio.h>
-#define PI 3.14
 
+#define PI 3.14
 void Test1(void)
 {
     double r;
@@ -21,26 +21,43 @@ void Test1(void)
     s = r * r * PI;
     printf("Circular area: %.2lf\n", s);
 }
-// 求个体积
-#define R 6613
+
+// 宏定义支持嵌套，求个地球体积
+#define R 6371
+#define V PI * R * R * R * 4 / 3
+void Test2(void)
+{
+    printf("Earth's volume is %f\n", V);
+}
 
 // 带参数的宏定义
 #define MAX(x, y) (((x) > (y)) ? (x) : (y))
-void Test2(void)
+void Test3(void)
 {
     int x, y;
     printf("Please enter two int:");
     scanf("%d%d", &x, &y);
-    printf("%d is the Max", MAX(x, y));
+    printf("%d is the Max\n", MAX(x, y));
 }
 
-#define squre(x) ((x) * (x))
-void Test3(void)
+#define SQUARE(x) ((x) * (x))
+void Test4(void)
 {
-    
+    double x;
+    int y;
+    printf("Please enter a double: ");
+    scanf("%lf", &x);
+    printf("Square of double is %lf\n", SQUARE(x));
+    printf("Please enter a int: ");
+    scanf("%d", &y);
+    printf("Square of int is %d\n", SQUARE(y));
 }
+
 int main()
 {
     Test1();
+    Test2();
+    Test3();
+    Test4();
     return 0;
 }
