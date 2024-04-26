88beca7f479a818741ed8802e187896de3d950ab
xiaoshunjie <xiaoshunjie@qq.com>
2024-04-26 23:41:38 +0800

    温习数据结构

---
+++ datastruct/3_linetypetable/p8_sqlist.c
@@ -0,0 +1,18 @@
+#define MAXSIZE 20
+
+typedef int ElemType;
+typedef int Status;
+
+typedef struct
+{
+    ElemType data[MAXSIZE];
+    int length;               // 线性表当前的长度
+}SqList;
+
+/**
+ * 
+*/
+Status GetElem(SqList L, int i, ElemType *e)
+{
+
+}
