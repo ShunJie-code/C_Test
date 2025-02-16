bca41afaed30a8dcc79dd0d57151e52310046e9a
xiaoshunjie <xiaoshunjie@qq.com>
2025-02-16 23:23:28 +0800

    完成图邻接矩阵和邻接表

--- datastruct/7_graph/graph.h
+++ datastruct/7_graph/graph.h
@@ -1,3 +1,13 @@
+/**
+ * @file graph.h
+ * @author your name (you@domain.com)
+ * @brief 通过邻接矩阵、邻接表，表示无向图和有向图
+ * @version 0.1
+ * @date 2025-02-16
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
 #ifndef GRAPH_H
 #define GRAPH_H
 
@@ -7,18 +17,39 @@
 typedef char VertexType; // 顶点类型定义
 typedef int EdgeType;
 
-/**
- * @brief 方法：邻接矩阵、邻接表
- * 目的：表示无向图和有向图
- */
+// region 邻接矩阵
 typedef struct
 {
-    VertexType vexs[MAXVEX]; // 顶点表
-    EdgeType arc[MAXVEX][MAXVEX]; // Adjacency Matrix
+    VertexType vexs[MAXVEX];        // 顶点表
+    EdgeType arc[MAXVEX][MAXVEX];   // 边表，Adjacency Matrix
     int numVertexes, numEdges;
 } MGraph;
 
 void CreateMGraph(MGraph *g);
+// endrigion 邻接矩阵
 
+
+// region 邻接表
+typedef struct EdgeNode
+{
+    int adjvex;
+    EdgeType weight;
+    struct EdgeNode *next;
+}EdgeNode;
+
+typedef struct VertexNode
+{
+    VertexType data;
+    EdgeNode *firstegde;
+}VertexNode, AdjList[MAXVEX]; // 重定义了MAXVEX个结构体的数组类型
+
+typedef struct 
+{
+    AdjList adjList;
+    int numVert;
+    
+}GraphAdjList;
+
+// endregion 邻接表
 
 #endif
